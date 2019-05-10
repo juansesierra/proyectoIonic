f5a7de7a5e564f719ddf63ec2e0ae928199504f2
Juan Sebastian Sierra <juan.sierra@airenetworks.es>
2019-05-10 12:34:44 +0200

    pagina de inicio estatica

--- src/app/inicio/inicio.page.ts
+++ src/app/inicio/inicio.page.ts
@@ -7,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioPage implements OnInit {
 
-  readonly eventos:string [] = ['Marmarela','Club Concerto','Magma Club','Marearock Bar','Havana']
-  constructor() { }
+  readonly imagenes: string [] = ['Marmarela', 'Club Concerto', 'Magma Club', 'Marearock Bar', 'Havana Castaños'];
+  readonly fechas: string [] = ['Viernes 10', 'Sabado 11'];
+   
+  eventos = [];
+
+
+  constructor() {
+   }
 
   ngOnInit() {
+    for (let index = 0; index < this.imagenes.length; index++) {
+      let fecha;
+
+      // asignamos la primera mitad una fecha y la otra mitad la otra
+      index < (this.imagenes.length / 2) ? fecha = 0 : fecha = 1;
+
+      this.eventos[index] =  {
+        'club' : this.imagenes[index],
+        'fecha' : this.fechas[fecha]
+      };
+    }
   }
 
 }
